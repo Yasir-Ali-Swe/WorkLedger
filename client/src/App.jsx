0de1e9521c942aba6eaed14806389637ee4a9b97
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-21 02:48:18 +0500

    Setup the Frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center bg-purple-800'>
+      <h1 className='text-gray-300 underline text-4xl font-black'>Welcome to WorkLedger</h1>
+    </div>
+  )
+}
+
+export default App
