51d2e9ec7cd91f8da991b63834d3814b4d755a7f
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-21 18:30:39 +0500

    Entry Point

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 import express from "express";
-import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDatabase } from "./config/database-connection.js";
+import { PORT } from "./config/env.js";
 
 const server = express();
-const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+// Server middlewares
+server.use(express.json());
+server.use(cookieParser());
+server.use(cors());
+
+
+// Server start
+server.listen(PORT, async () => {
+  await connectDatabase();
   console.log(`Server started on port ${PORT}`);
 });
