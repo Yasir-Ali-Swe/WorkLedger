dd59040dd92691194704c9920b6248e2b99ffe92
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-21 20:06:37 +0500

    Server Configurations

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 
 const PORT = process.env.PORT || 5000;
-const DATABASE_STRING = process.env.DATABASE_STRING 
-
-export { PORT , DATABASE_STRING };
+const DATABASE_STRING = process.env.DATABASE_STRING;
+const USER_GMAIL = process.env.USER_GMAIL;
+const Email_Password = process.env.Email_Password;
+export { PORT, DATABASE_STRING, USER_GMAIL, Email_Password };
