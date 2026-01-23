ddad6abe47cc42cc4044848b832a861429ce0885
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-23 22:18:55 +0500

    Project Model

---
+++ backend/models/project.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    teamMembers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const projectModel = mongoose.model("Project", projectSchema);
+
+export default projectModel;
