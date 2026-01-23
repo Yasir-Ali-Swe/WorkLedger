0f27a801cf89e894ea57f20b4560d44ab8d38e1b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-23 22:18:07 +0500

    Comment Model

---
+++ backend/models/comment.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    taskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const commentModel = mongoose.model("Comment", commentSchema);
+
+export default commentModel;
