a2d3846d35620b75518768d603fcf7b2ed249979
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-23 22:16:09 +0500

    Logs Model

---
+++ backend/models/logs.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const logSchema = new mongoose.Schema(
+  {
+    actionType: {
+      type: String,
+      enum: [
+        "USER_LOGIN",
+        "PROJECT_CREATED",
+        "TASK_ASSIGNED",
+        "TASK_STATUS_UPDATED",
+        "INVITATION_SENT",
+        "INVITATION_ACCEPTED",
+        "COMMENT_ADDED",
+      ],
+    },
+    actorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    actorRole: {
+      type: String,
+      enum: ["PROJECT_MANAGER", "TEAM_MEMBER","SYSTEM"],
+      required: true,
+    },
+    entityType: {
+      type: String,
+      enum: ["PROJECT", "TASK", "INVITATION", "COMMENT", "USER"],
+    },
+    entityId: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "entityType",
+    },
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+    },
+    metadata: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+  },
+  { timestamps: true },
+);
+
+const logModel = mongoose.model("Log", logSchema);
+export default logModel;
