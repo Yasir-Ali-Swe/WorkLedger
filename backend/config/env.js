dceaaa1c3b6d5699565c583954aaa22133adc932
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-25 13:49:38 +0500

    Environment vairables

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,4 +4,6 @@ const PORT = process.env.PORT || 5000;
 const DATABASE_STRING = process.env.DATABASE_STRING;
 const USER_GMAIL = process.env.USER_GMAIL;
 const Email_Password = process.env.Email_Password;
-export { PORT, DATABASE_STRING, USER_GMAIL, Email_Password };
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export { PORT, DATABASE_STRING, USER_GMAIL, Email_Password, JWT_SECRET };
